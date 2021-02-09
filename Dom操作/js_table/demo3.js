52dba53b397cb3540a7d638bf86b11540f4f39bd
UncleSweet <2577817403@qq.com>
2021-02-09 15:42:11 +0800

    Dom js_table demo3 change

--- Dom操作/js_table/demo3.js
+++ Dom操作/js_table/demo3.js
@@ -1,12 +1,12 @@
 window.onload  = function(){
-    console.log(typeof BrowserDetect);
+    console.log(document.styleSheets[1]);
 }
 
 var flag = true;
 
 if(flag){
     //loadCss('../css/demo3.css');
-    //loadCss('../css/demo2.css');
+    loadCss('../css/demo2.css');
     loadStyle();
 }
 
@@ -30,20 +30,35 @@ function loadCss(url){
 //动态执行style
 function loadStyle(){
     var style = document.createElement('style');
+    style.setAttribute('type','text/css');
     document.getElementsByTagName('head')[0].appendChild(style);
+
     //style.appendChild(insertRule(document.styleSheets[0],'#box','width:200px;height:200px;background-color:red',0));
-    insertRule(document.styleSheets[0],'#box','width:200px;height:200px;background-color:red',0);
+
+    var rule1 = insertRule(document.styleSheets[1],'#box','width:200px;height:200px;background-color:red',0).content;
+    var rule2 = insertRule(document.styleSheets[1],'#pox','width:300px;height:300px;background-color:blue',1).content;
+
+    style.appendChild(document.createTextNode(rule1));
+    style.appendChild(document.createTextNode(rule2));
+
+    //console.log(head.lastChild);
+    console.log(rule1);
+    console.log(rule2);
 }
 
 //兼容ie、非ie加载css代码
 function insertRule(sheet,selectorText,cssText,position){
     //如果是非ie
     if(sheet.insertRule){
-        sheet.insertRule(selectorText+"{"+ cssText+"}",position);   
+        sheet.insertRule(selectorText+"{"+ cssText +"}",position);   
     }
     //如果是ie
     else if(sheet.addRule){
         sheet.addRule(selectorText,cssText,position);
     }
+
+    return {
+        content : selectorText+"{"+cssText+"}"
+    };
 }
 
